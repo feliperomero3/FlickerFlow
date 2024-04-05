cfe2a4b6cd37f8a5572a25eff406a704f9a46f1a
Felipe Romero <feliperomero3@outlook.com>
2024-04-04 23:59:40 -0600

    Adding streaming video.

--- src/src/index.js
+++ src/src/index.js
@@ -1,12 +1,32 @@
 const express = require('express')
+const fs = require('fs')
+
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 3000
 
 const app = express()
-const port = 3000
+app.set('appname', 'FlickerFlow')
+app.set('port', port)
+app.set('host', host)
+
+app.get('/video', async (req, res) => {
+  const path = 'src/assets/sample.mp4'
+
+  // Note that we're using "fs.promises" instead of "fs" to
+  // gain access to filesystem functions that are compatible
+  // with JavaScript's async/await keywords.
+  const stats = await fs.promises.stat(path)
+  res.writeHead(200, {
+    'Content-Length': stats.size,
+    'Content-Type': 'video/mp4'
+  })
+  fs.createReadStream(path).pipe(res)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`First example listening on port ${port}.`)
+  console.log(`FlickerFlow listening at http://${host}:${port}`)
 })
